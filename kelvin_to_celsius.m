53d3cf55fea4f60250cf21794231f70274b7975d
Yuxiang Meng <Yuxiang.meng@postgrad.manchester.ac.uk>
2020-11-14 22:32:37 +0800

    added the title

--- kelvin_to_celsius.m
+++ kelvin_to_celsius.m
@@ -1,5 +1,5 @@
 function celsius = kelvin_to_celsius(kelvin)
     %KELVIN_TO_CELSIUS   Convert Kelvin to degrees Celsius
-
     celsius = kelvin - 273.15
+    fprintf('Absolute zero is 0K, which is %g%sC.\n', kelvin_to_celsius(0), deg)
 end
