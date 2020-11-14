50c2e74d9221a8c2b1e3b20271337d82d61aa5a5
Yuxiang Meng <Yuxiang.meng@postgrad.manchester.ac.uk>
2020-11-14 22:32:04 +0800

    ss

---
+++ eee.m
@@ -0,0 +1,37 @@
+[33mcommit 94f699ea7b248407f295cf79b65300df8437c05f[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m)[m
+Author: Yuxiang Meng <Yuxiang.meng@postgrad.manchester.ac.uk>
+Date:   Sat Nov 14 22:17:06 2020 +0800
+
+    "Include Kelvin conversion"
+
+[33mcommit 1a4d3b159c0525bcd29dff807b177d765c26a203[m
+Author: Yuxiang Meng <Yuxiang.meng@postgrad.manchester.ac.uk>
+Date:   Sat Nov 14 20:43:26 2020 +0800
+
+    eee
+
+[33mcommit e7b4dd18f444f978fdced7f624352063634342e1[m
+Author: Yuxiang Meng <Yuxiang.meng@postgrad.manchester.ac.uk>
+Date:   Sat Nov 14 20:38:14 2020 +0800
+
+    'Add script to print key conversions'
+
+[33mcommit 1e696f5378842078b8c2f7609cf38ef8f1c25811[m
+Author: Yuxiang Meng <Yuxiang.meng@postgrad.manchester.ac.uk>
+Date:   Sat Nov 14 20:30:49 2020 +0800
+
+    Add functions to convert between C and F
+
+[33mcommit 401fa0b27769e1154876ba5f7a2dc9ad2de57978[m
+Author: Yuxiang Meng <Yuxiang.meng@postgrad.manchester.ac.uk>
+Date:   Sat Nov 14 20:23:47 2020 +0800
+
+    Add kelvin to celsius conversion
+
+[33mcommit 0d15621f11edd1d36db204ef42e4723e8ac2406f[m
+Author: Yuxiang Meng <Yuxiang.meng@postgrad.manchester.ac.uk>
+Date:   Sat Nov 14 20:15:34 2020 +0800
+
+    function celsius = kelvin_to_celsius(kelvin)
+        celsius = kelvin - 273.15
+    end
